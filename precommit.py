03aefb881978b607926e66e6376fa757cfdb5b22
Marko Ristin <marko.ristin@gmail.com>
2018-11-13 14:26:00 +0100

    precommit tests changelog version mismatch (#45)

--- precommit.py
+++ precommit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Run all pre-commit checks."""
-
+import os
 import pathlib
 import subprocess
 import sys
@@ -10,7 +10,7 @@ import sys
 def main() -> int:
     """"Exectute main routine."""
 
-    here = pathlib.Path(".")
+    here = pathlib.Path(os.path.abspath(__file__)).parent
 
     pths = sorted(here.glob("*.go"))
 
@@ -30,21 +30,41 @@ def main() -> int:
 
     packages = [pkg for pkg in packages if 'vendor' not in pkg.parents]
 
-    subprocess.check_call(['go', 'vet'] + [pkg.as_posix() for pkg in packages])
+    subprocess.check_call(['go', 'vet'] + [pkg.as_posix() for pkg in packages], cwd=here.as_posix())
 
-    subprocess.check_call(['golint'] + [pkg.as_posix() for pkg in packages])
+    subprocess.check_call(['golint'] + [pkg.as_posix() for pkg in packages], cwd=here.as_posix())
 
-    subprocess.check_call(['errcheck'] + [pkg.as_posix() for pkg in packages])
+    subprocess.check_call(['errcheck'] + [pkg.as_posix() for pkg in packages], cwd=here.as_posix())
 
     non_test_pths = [pth for pth in pths if not pth.name.endswith("_test.go" or "testcases" in pth.parents)]
     subprocess.check_call(["gocyclo", "-over", "15"] + [pth.as_posix() for pth in non_test_pths])
 
     for pkg in packages:
         subprocess.check_call(['go', 'test', '-v', '-covermode=count',
-                               '-coverprofile=../../../{}/profile.coverprofile'.format(pkg), pkg.as_posix()])
+                               '-coverprofile=../../../{}/profile.coverprofile'.format(pkg), pkg.as_posix()],
+                              cwd=here.as_posix())
 
-    subprocess.check_call(['go', 'build', './...'])
+    subprocess.check_call(['go', 'build', './...'], cwd=here.as_posix())
 
+    ##
+    # Check that the CHANGELOG.md is consistent with -version
+    ##
+    version = subprocess.check_output(['go', 'run', 'main.go', '-version'], cwd=here.as_posix(),
+                                      universal_newlines=True).strip()
+
+    changelog_pth = here / "CHANGELOG.md"
+    changelog_lines = changelog_pth.read_text().splitlines()
+    if len(changelog_lines) < 1:
+        raise RuntimeError("Expected at least a line in {}, but got: {} line(s)".format(
+            changelog_pth, len(changelog_lines)))
+
+    # Expect the first line to always refer to the latest version
+    changelog_version = changelog_lines[0]
+
+    if version != changelog_version:
+        raise AssertionError(("The version in changelog file {} (parsed as its first line), {!r}, "
+                              "does not match the output of -version: {!r}").format(
+            changelog_pth, changelog_version, version))
 
 if __name__ == "__main__":
     sys.exit(main())
